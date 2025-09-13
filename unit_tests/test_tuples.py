2c488395d38c8f6d905f061c656f9655400ea899
praerie <elisabethadelie@gmail.com>
2025-09-13 16:23:50 -0400

    improve tuple comparisons and add tuple equality test

--- unit_tests/test_tuples.py
+++ unit_tests/test_tuples.py
@@ -2,7 +2,7 @@ from src.utils.math_utils import equal
 import src.tuples as tuples
 
 def test_point():
-    """Scenario: A tuple with w=1.0 is a point."""
+    """A tuple with w=1.0 is a point."""
     p = tuples.point(1, 2, 3)
     assert equal(p.x, 1)
     assert equal(p.y, 2)
@@ -12,7 +12,7 @@ def test_point():
     assert not p.is_vector
 
 def test_vector():
-    """Scenario: A tuple with w=0.0 is a vector."""
+    """A tuple with w=0.0 is a vector."""
     v = tuples.vector(4, 5, 6)
     assert equal(v.x, 4)
     assert equal(v.y, 5)
@@ -20,3 +20,14 @@ def test_vector():
     assert equal(v.w, 0.0)
     assert v.is_vector
     assert not v.is_point
+
+def test_tuple_equality():
+    """Tuples with the same values should be equal."""
+    t1 = tuples.point(1, 2, 3)
+    t2 = tuples.point(1, 2, 3)
+    t3 = tuples.vector(1, 2, 3)
+    t4 = tuples.vector(4, 5, 6)
+
+    assert t1 == t2
+    assert not t1 == t3
+    assert not t3 == t4
